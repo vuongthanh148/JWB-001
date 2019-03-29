9dc330780fef821ce2e333fdb2e32e652413fc42
andy <ducanh54@gmail.com>
2019-03-29 09:08:27 +0700

    http servlet demo

---
+++ mentor_le_duc_anh/java_servlet/src/main/java/com/uetcodecamp/js/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.uetcodecamp.js;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MyServlet",
+        urlPatterns = {"/"}
+)
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+
+        out.write("Hello world!".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
